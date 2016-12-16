4d0afa89928807d9261dfff34fb7632ea662c66e
Lorenzo Caldara <lorenzo@Lorenzos-MacBook-Air.local>
2016-12-16 15:20:07 +0100

    Primo commit

---
+++ config.rb
@@ -0,0 +1,87 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+activate :blog do |blog|
+    blog.name = 'blog'
+    blog.default_extension = ".erb"
+
+    blog.tag_template = "tag.html"
+    blog.calendar_template = "calendar.html"
+    blog.sources = "/blog/{category}/{year}-{month}-{day}-{title}.html"
+    blog.permalink = "/blog/{category}/{year}-{month}-{day}-{title}.html"
+
+    # Enable pagination
+    blog.paginate = true
+    blog.per_page = 10
+    blog.page_link = "page/{num}"
+
+    blog.custom_collections = {
+        category: {
+            link: '/blog/categories/{category}.html',
+            template: '/category.html'
+        }
+    }
+end
+
+# Change Compass configuration
+compass_config do |config|
+  config.add_import_path "bower_components/foundation-sites/scss/"
+  config.output_style = :compact
+
+  # Set this to the root of your project when deployed:
+  config.http_path = "/"
+  config.css_dir = "stylesheets"
+  config.sass_dir = "stylesheets"
+  config.images_dir = "images"
+  config.javascripts_dir = "javascripts"
+end
+
+activate :sprockets
+# Add bower's directory to sprockets asset path
+after_configuration do
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
+
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
