01950680b23dc5790dc3013cad740d903f214f5f
Lorenzo Caldara <lorenzo@Lorenzos-MacBook-Air.local>
2017-01-10 18:18:33 +0100

    Inseriti javascript

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,6 @@
-// This is where it all goes :)
+//= require jquery/dist/jquery
+//= require what-input/what-input.min
+//= require foundation-sites/dist/foundation.min
+
+//= require app
+//= require dati_form
