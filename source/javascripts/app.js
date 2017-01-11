f6943518b5381a5c7439bcfe39672d7e537d531f
Lorenzo Caldara <lorenzo@Lorenzos-MacBook-Air.local>
2017-01-11 18:03:06 +0100

    Corretto evento acquistaSubito su classe e non su ID

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,7 +1,7 @@
 $(document).foundation();
 
 
-$( "#acquistaSubito" ).on( "click", function() {
+$( ".acquistaSubito" ).on( "click", function() {
     ga('send', {hitType: 'event', eventCategory: 'Bottone', eventAction: 'click', eventLabel: 'acquistaSubito'});
     console.log ('acquistaSubito');
 });
