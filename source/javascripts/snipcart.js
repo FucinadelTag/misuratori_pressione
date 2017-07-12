499a6464e6418437aea8d66735e9ff47a45514d1
Lorenzo Caldara <lorenzo@dinoweb.net>
2017-07-12 14:08:31 +0200

    Test sul carrello

--- source/javascripts/snipcart.js
+++ source/javascripts/snipcart.js
@@ -1,11 +1,25 @@
 Snipcart.api.configure('split_firstname_and_lastname', false);
 
 Snipcart.subscribe('cart.opened', function (data) {
+    Snipcart.unsubscribe('cart.opened');
     console.log ('aperto');
     $('#snipcart-discounts').hide();
 });
 
-Snipcart.subscribe('page.change', function (data) {
-    console.log ('cambia');
-    $('#snip-actions').hide();
-});
+// Snipcart.subscribe('page.change', function (data) {
+//     console.log (data);
+//     Snipcart.unsubscribe('cart.change');
+
+//     $( document ).ready(function() {
+//         $('#snipcart-discounts').hide();
+//         if (data == 'payment-method') {
+//             var element = $('.snip-actions').detach();
+//             console.log (element.html());
+//             $('#snipcart-cvc-content-holder').append(element);
+//         }
+//     });
+
+
+
+
+// });
